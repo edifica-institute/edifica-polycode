788887cebcc4d839f1189649577d9f602a487343
Edifica <edifica.institute@gmail.com>
2025-08-19 23:06:55 +0530

    Update terminal.js

--- frontend/js/core/terminal.js
+++ frontend/js/core/terminal.js
@@ -10,7 +10,7 @@ term = new Terminal({
   theme: {
     background: '#0b1220',    // matches your app background
     foreground: '#e5e7eb',
-    cursor: 'bar',
+    cursor: 'underline',
     selection: '#334155aa',
   },
 });
